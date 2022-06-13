e3408f664203deaee5513d47d51b79728b38c237
liuzhiyu <lzy20190501@gmail.com>
2022-06-13 12:09:36 +0800

    update

---
+++ HarrisCornerDetector/HarrisCorner.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <chrono>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    cout<<"HelloWorld"<<endl;
+    return 0;
+}
+
+
+
+// vector<pair<pair<float, float>, pair<float, float>>>
+
---
+++ HarrisCornerDetector/build/CMakeFiles/harris.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/harris.dir/HarrisCorner.cpp.o"
+  "harris"
+  "harris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/harris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
